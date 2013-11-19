53ca6f0cf36b41daa8756cca9257256fc8269230
Sadra <Mojtaba Elahi>
2013-11-19 09:03:39 +0330

    runPlanner is getting better

--- myRun/runPlanner.sh
+++ myRun/runPlanner.sh
@@ -5,15 +5,17 @@
 
 
 
-Planner="POPF"
 Planner="LiftedRangedModaber"
 Planner="LiftedSMTModaber"
+Planner="POPF"
 
-Domain=( 'market');
-DomainFile=( 'domain.pddl' );
 
-Domain=( 'Satellite' 'Depots' 'ZenoTravel' 'DriverLog' 'Rovers');
-DomainFile=( 'metricSat.pddl' 'DepotsNum.pddl' 'zenonumeric.pddl' 'driverlogNumeric.pddl' 'NumRover.pddl');
+Domain=( 'market' 'Satellite' 'Depots' 'ZenoTravel' 'DriverLog' 'Rovers');
+DomainFile=( 'domain.pddl' 'metricSat.pddl' 'DepotsNum.pddl' 'zenonumeric.pddl' 'driverlogNumeric.pddl' 'NumRover.pddl');
+
+Domain=( 'market' 'Satellite' );
+DomainFile=( 'domain.pddl' 'metricSat.pddl' );
+
 
 
 
@@ -26,13 +28,17 @@ for (( j = 0; j < ${#Domain[*]} ; j++)) {
 }
 
 for (( i = 1; i <= 20; i++)) {
-#Depots domain has 22 problems but we just try for first 20 problems of it; so we should try for other 2 problem later!
 	for (( j = 0; j < ${#Domain[*]} ; j++)) {
+		
+		if [[ ${Domain[j]} == "market" ]]  && (( i > 10 )) ; then
+			continue;
+		fi
+
 		TheDomainFile="../../Problem/ipc2002/Tests1/${Domain[$j]}/Numeric/${DomainFile[$j]}"
 		TheProblemFile="../../Problem/ipc2002/Tests1/${Domain[$j]}/Numeric/pfile$i"
 		echo "Planner \"$Planner\" try to solve: $TheDomainFile $TheProblemFile"   
-		timeout 30m ./runModaber.sh "$TheDomainFile" "$TheProblemFile" > "${Planner}Results/${Domain[$j]}/pfile$i.output" 2>&1
-#		timeout 30m ./optic-clp -N "$TheDomainFile" "$TheProblemFile" > "${Planner}Results/${Domain[$j]}/pfile$i.output" 2>&1
+#		timeout 30m ./runModaber.sh "$TheDomainFile" "$TheProblemFile" > "${Planner}Results/${Domain[$j]}/pfile$i.output" 2>&1
+		timeout 30m ./optic-clp -N "$TheDomainFile" "$TheProblemFile" > "${Planner}Results/${Domain[$j]}/pfile$i.output" 2>&1
 		if [ -f solution ]; then
 			mv solution "${Planner}Results/${Domain[$j]}/pfile$i.solution"
 		fi
