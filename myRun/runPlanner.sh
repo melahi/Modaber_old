d01f59effa6e016cb131f744b95152f17bb76698
Sadra <Mojtaba Elahi>
2014-01-08 21:09:30 +0330

    someMinorChang

--- myRun/runPlanner.sh
+++ myRun/runPlanner.sh
@@ -9,7 +9,7 @@ Planner="LiftedRangedModaber"
 Planner="LiftedSMTModaber"
 Planner="POPF"
 
-Planner="PreferenceMyPF"
+Planner="PreferenceMyFP"
 
 Domain=( 'market' 'Satellite' 'Depots' 'ZenoTravel' 'DriverLog' 'Rovers');
 DomainFile=( 'domain.pddl' 'metricSat.pddl' 'DepotsNum.pddl' 'zenonumeric.pddl' 'driverlogNumeric.pddl' 'NumRover.pddl');
@@ -21,8 +21,8 @@ DomainFile=( 'domain.pddl' 'metricSat.pddl' );
 Domain=( 'pegsol-strips' 'elevators-numeric' 'transport-numeric' 'elevators-strips' 'woodworking-numeric');
 
 #Set maximum memory limit
-MaximumMemoryLimit=$((1 * 1024 * 1024))
-ulimit -v $MaximumMemoryLimit
+#MaximumMemoryLimit=$((1 * 1024 * 1024))
+#ulimit -v $MaximumMemoryLimit
 
 for (( j = 0; j < ${#Domain[*]} ; j++)) {
 	mkdir -p "${Planner}Results/${Domain[$j]}"
